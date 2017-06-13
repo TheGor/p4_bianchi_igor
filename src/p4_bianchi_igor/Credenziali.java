55018fcf8fbb4fb4cd094cc311c683213cb2af2f
Igor <igor.system@hotmail.it>
2017-06-13 22:02:14 +0200

    cancellazione consegna per riprovare a caricarla

--- src/p4_bianchi_igor/Credenziali.java
+++
@@ -1,9 +0,0 @@
-package p4_bianchi_igor;
-/**
- * Classe per definire l'oggetto Credenziali
- */
-public class Credenziali{
-    /** Attributi */
-    private String username;
-    private String password;
-}
